a40f10734ac5843ad4c5fe14ee3a7c69e651617c
ryongseong <xmssnsk2119@gmail.com>
2024-08-24 11:30:28 +0900

    Add - 시험 관련 모델 추가

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Integer, String, Column, Text, DateTime, ForeignKey
+from sqlalchemy import Boolean, Integer, String, Column, Text, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
-
 from database import Base
+from datetime import datetime
 
 class Question(Base):
     __tablename__ = "question"
@@ -10,7 +10,7 @@ class Question(Base):
     category = Column(String, nullable=True)
     subject = Column(Text, nullable=False)
     content = Column(Text, nullable=False)
-    create_date = Column(DateTime, nullable=False)
+    create_date = Column(DateTime, nullable=False, default=datetime.now())
     user_id = Column(Integer, ForeignKey("user.id"))
     user = relationship("User", back_populates="questions")
     modify_date = Column(DateTime, nullable=True)
@@ -25,3 +25,48 @@ class User(Base):
     thread_id = Column(String, nullable=True)
 
     questions = relationship("Question", back_populates="user")
+    exams = relationship("Exam", back_populates="user")
+    attempts = relationship("Attempt", back_populates="user")
+
+class Exam(Base):
+    __tablename__ = "exam"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    description = Column(Text, nullable=True)
+    create_date = Column(DateTime, default=datetime.now())
+    user_id = Column(Integer, ForeignKey("user.id"))
+    user = relationship("User", back_populates="exams")
+    questions = relationship("TestQuestion", back_populates="exam")
+
+class TestQuestion(Base):
+    __tablename__ = "test_question"
+
+    id = Column(Integer, primary_key=True)
+    exam_id = Column(Integer, ForeignKey("exam.id"))
+    content = Column(Text, nullable=False)
+
+    exam = relationship("Exam", back_populates="questions")
+    choices = relationship("Choice", back_populates="question")
+
+class Choice(Base):
+    __tablename__ = "choice"
+
+    id = Column(Integer, primary_key=True)
+    question_id = Column(Integer, ForeignKey("test_question.id"))
+    content = Column(Text, nullable=False)
+    is_correct = Column(Boolean, nullable=False)
+
+    question = relationship("TestQuestion", back_populates="choices")
+
+class Attempt(Base):
+    __tablename__ = "attempt"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    exam_id = Column(Integer, ForeignKey("exam.id"))
+    score = Column(Integer, nullable=False)
+    attempt_date = Column(DateTime, default=datetime.now())
+
+    user = relationship("User", back_populates="attempts")
+    exam = relationship("Exam")
