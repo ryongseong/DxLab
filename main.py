178bbd523aa3e76e537eb16051cdd7fc3b2667a0
ryongseong <xmssnsk2119@gmail.com>
2024-08-24 11:30:09 +0900

    Add - exam_router 연결

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 from domain.user import user_router
 from domain.question import question_router
+from domain.exam import exam_router
 
 app = FastAPI()
 
@@ -21,3 +22,4 @@ app.add_middleware(
 
 app.include_router(user_router.router)
 app.include_router(question_router.router)
+app.include_router(exam_router.router)
