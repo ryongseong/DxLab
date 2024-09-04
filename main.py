d4b49279d26d9dc2b3c276e4b4673ce587e03148
임성룡 <ryong@ryong-3.local>
2024-09-04 17:55:21 +0900

    Add - Gemini Router 추가

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from starlette.middleware.cors import CORSMiddleware
 from domain.user import user_router
 from domain.question import question_router
 from domain.exam import exam_router
+from domain.gemini import gemini_router
 
 app = FastAPI()
 
@@ -23,3 +24,4 @@ app.add_middleware(
 app.include_router(user_router.router)
 app.include_router(question_router.router)
 app.include_router(exam_router.router)
+app.include_router(gemini_router.router)
